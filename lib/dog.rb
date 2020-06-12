3548c7f57269d7eec2c05eb246ea9334c58fe496
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-12 21:02:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :owner
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+  @name = name
+  @owner = owner 
+  @@all << self 
 end
