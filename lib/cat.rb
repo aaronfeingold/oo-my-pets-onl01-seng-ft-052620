2b305a3469a95f7849dd33cac702bd3b342d625e
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-12 20:58:58 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :owner
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+  end 
+  
 end
