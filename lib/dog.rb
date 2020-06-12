e1777bea7ddaf01d565501a20a3ae60c9672d263
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-12 21:03:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog
-  attr_accessor :owner
+  attr_accessor :owner, :mood 
   attr_reader :name 
   
   @@all = []
   
   def initialize(name, owner)
     @name = name
+    @mood = "nervous"
     @owner = owner 
     @@all << self 
   end 
